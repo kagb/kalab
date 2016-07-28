ec8d7dffbbcaabfe46f32cb249bf3db9b8aac55b
kagb <ka@evilka.com>
2016-07-28 14:30:23 +0800

    add font family

--- static/src/js/base.js
+++ static/src/js/base.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Kalab from './Kalab.jsx';
 
-ReactDOM.render(<App />, document.getElementById('Kalab'));
+ReactDOM.render(<Kalab />, document.getElementById('kalab'));
