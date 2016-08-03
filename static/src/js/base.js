013a4138ee39321aaefe657d4bdd825073f83106
kagb <ka@evilka.com>
2016-08-03 17:10:24 +0800

    update home base js

--- static/src/js/base.js
+++ static/src/js/base.js
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var CenterText = React.createClass({
+var i_names = ['ABOUT', 'BLOG', 'TODAY', 'NLP-T', 'GOLANG', 'SPIDER'];
+
+var IndexTag = React.createClass({
   render: function(){
-    return (<div class="center-txt"> {this.props.txt} </div>);
+    return (<li className="i-tag">{this.props.tag_name}</li>)
   }
 });
 
-ReactDOM.render(<CenterText txt="Hi, this is Ka'Lab" />, document.getElementById('kalab'));
+var Tags = React.createClass({
+  render: function(){
+    return (
+    <div className="tags">
+      <ui>
+        {
+          i_names.map(function (name){
+            return <IndexTag tag_name={name} />
+          })
+        }
+      </ui>
+    </div>
+    )
+  }
+});
+
+var Main = React.createClass({
+  render: function(){
+    return (
+      <div className="page">
+        <div className="m-txt"> {this.props.txt} </div>
+        <Tags />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Main txt="Hi, this is Ka'Lab" />, document.getElementById('kalab'));
