81c56063d9502d97499ee044f7ef000e2b28b446
kagb <ka@evilka.com>
2016-09-27 14:42:27 +0800

    fishing qq

---
+++ views/lab.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from flask import Blueprint, render_template
+
+__all__ = ['BP_LAB']
+
+BP_LAB = Blueprint('lab', __name__)
+
+
+@BP_LAB.route('/fishing_qq')
+def fishing_qq():
+    return render_template('lab/fishing_qq.html')
