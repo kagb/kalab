9dd77b422b91b8f0bfdc5af73a9023ca9a64db06
kagb <ka@evilka.com>
2016-09-29 11:58:31 +0800

    fix pv count

--- apis/pv.py
+++ apis/pv.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import request
+from flask import abort, request
 from flask_restful import Resource
 
 from lib.redis_store import redis_store
@@ -9,12 +9,15 @@ __all__ = ['PagePV']
 
 
 REDIS_PAGES_PV_KEY = 'reids-pages-pv-key-v1'
+ALLOW_PAGES = [u'index']
 
 
 class PagePV(Resource):
 
     def get(self):
         page = request.args.get('page', u'index')
+        if page not in ALLOW_PAGES:
+            abort(400)
         name = REDIS_PAGES_PV_KEY
         pv = redis_store.hset_or_incr(name, page, 1)
         return {'page': page, 'pv': int(pv)}
